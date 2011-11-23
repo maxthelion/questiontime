cb61bc8a1ddc3e006b613df30116bd7385b36361
Max Williams <max@new-bamboo.co.uk>
2011-11-23 17:01:36 +0000

    More random stuff

--- server/local_server.js
+++ server/local_server.js
@@ -3,23 +3,34 @@ var client = Pipe.createClient({
     key: '2b4dbdeb570fc373332f',
     secret: '46edda14a70079d756e8',
     app_id: 66,
-    debug: true
+    debug: false
 });
 
+client.subscribe(['socket_message', 'socket_existence'])
+
+function randOrd(){
+  return (Math.round(Math.random())-0.5); 
+}
+
+function near(n){
+  var variance = Math.random() * 20;
+  return Math.floor(n + variance);
+}
 
 function generateQuestion(){
-  var a = Math.floor( Math.random() * 10 )
-  var b = Math.floor( Math.random() * 10 )
+  var a = Math.floor( Math.random() * 100 )
+  var b = Math.floor( Math.random() * 100 )
   var question = a + "+" + b
   correctAnswer = eval(a + "+" + b)
+  var answers = [
+    near(correctAnswer),
+    near(correctAnswer),
+    near(correctAnswer),
+    correctAnswer 
+  ].sort( randOrd );
   return {
     question: question,
-    answers: [
-      Math.random() * 100, 
-      Math.random() * 100, 
-      Math.random() * 100,
-      correctAnswer 
-    ]
+    answers: answers
   }
 }
 
@@ -36,20 +47,21 @@ function sendToAdmin(eventName, data){
   }
 }
 
-client.subscribe(['socket_message', 'socket_existence'])
-
 var activeUsers = {}
 var currentQuestion = null;
 var admin = null
 var questionData = generateQuestion();
 
 client.sockets.on('close', function(socketId) {
+  if(activeUsers[socketId]) 
+    console.log('player left: ' + activeUsers[socketId].name )
   delete activeUsers[socketId];
   sendToAdmin('playerList', activeUsers)
 });
 
 // new player registering
 client.sockets.on('event:register', function(socketId, data){
+  console.log('player registered: ' + data.name )
   activeUsers[socketId] = {
     socketId: socketId,
     name: data.name,
@@ -61,24 +73,30 @@ client.sockets.on('event:register', function(socketId, data){
   sendToAdmin('newPlayer', activeUsers[socketId])
 })
 
+// Someone submitted an answer
 client.sockets.on('event:submitAnswer', function(socketId, data){
+  console.log(activeUsers[socketId].name +' answered '+ data.answer);
   if (data.answer == correctAnswer){
     activeUsers[socketId].score++
     sendToAdmin('winner', activeUsers)
     sendNewQuestion()
     sendToAdmin('newQuestion', questionData)
+    console.log('CORRECT! sending everyone a new question!');
   } 
 })
 
-// admin starting the game
+// admin moving to new question
 client.sockets.on('event:next', function(socketId, data){
+  console.log('NEXT QUESTION!!!');
   sendNewQuestion()
 })
 
+//Admin has connected, send them current players
 client.sockets.on('event:start', function(socketId, data){
   admin = socketId
   sendToAdmin('playerList', activeUsers)
   sendToAdmin('newQuestion', questionData)
+  console.log('Sending player list to admin');
 })
 
 client.connect();
